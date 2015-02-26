ef54b3db6c2ad88c58274087bd3729dfc6612e1b
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-02-26 13:45:23 -0700

    Completed exercise

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -8,4 +8,6 @@ class CatsController < ApplicationController
     @cat = Cat.find(params[:id])
   end
 
+  
+
 end
--- app/models/toy.rb
+++ app/models/toy.rb
@@ -2,4 +2,6 @@ class Toy < ActiveRecord::Base
 
   # add your belongs_to / has_many lines here
 
+  belongs_to :cat
+
 end
